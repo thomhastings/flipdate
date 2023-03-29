838c71aad8505fa2b91c56bc880bfe46d6c15f77
Thom Hastings <thomhastings@users.noreply.github.com>
2023-03-29 00:47:25 -0700

    use drag'n'drop method

--- update-flipper.sh
+++ update-flipper.sh
@@ -5,9 +5,37 @@
 # clone repo https://github.com/thomhastings/flipdate and run
 
 # update self
+echo "Updating self..."
 git pull
 
 # update submodules
+echo "Updating all submodules..."
 git submodule update --remote
 
-# TODO: mount SD card, copy files, prompt to eject
+# TODO: mount SD card, copy files, unmount, prompt user to eject
+# or better yet, just arrange some files to copy over drag'n'drop
+echo "Copying extra apps..."
+mkdir -p ./flipper-root
+cp -R ./all-the-plugins/apps ./flipper-root/
+echo "Copying BadUSB scripts..."
+mkdir -p ./flipper-root/badusb
+cp -R ./Flipper_Zero-BadUsb/BadUsb-Collection ./flipper-root/badusb/
+echo "Copying infrared database..."
+mkdir -p ./flipper-root/infrared
+cp -R ./Flipper-IRDB/* ./flipper-root/infrared/
+echo "Copying music files..."
+mkdir -p ./flipper-root/music_player
+cp -R ./flipper-music-files/* ./flipper-root/music_player/
+echo -e "Done.\nCopy the contents of /flipper-root/ to root of SD card.\n"
+
+# push update back to repo
+echo "Committing submodule changes..."
+git commit -a -m "updated with script"
+
+# run in VScode or this won't work
+echo "Pushing changes back to GitHub (only works in VScode terminal)."
+git push
+
+# cleanup (but only after copying has taken place)
+#rm -rf flipper-zero
+# Q: if a file is deleted from the repo, how to delete from Flipper Zero itself?
